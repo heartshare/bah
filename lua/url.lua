b59f41ad8e2c1bda81ca364e085907e6384d1350
Jeffry Lunggot <jeffry@bayau.com>
2016-11-04 11:07:32 +0800

    check for existing record before save

--- lua/url.lua
+++ lua/url.lua
@@ -1,30 +1,28 @@
 local config = require 'config'
+local util   = require 'util'
 local db     = require 'db'
-
--- generate random string
-local shorten = function(long_url)
-  math.randomseed(os.time() + math.random(0, 100000))
-  local s = ""
-  local cz= {"a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "v", 
-             "m", "n", "o", "p", "q", "r", "s", "t", "w", "x", "y", "z" }
-  for _ = 1, 6, 1 do
-    local index = math.random(1, #cz) 
-    s = s .. cz[index] 
-    end
-  return s
-end
 
 local args = ngx.req.get_uri_args()
 local long_url = args['url']
 
 -- TODO: verify url format
-if not long_url then return ngx.say('Missing Url') end
 
-local short_url = shorten(long_url) 
+if not long_url then 
+  return ngx.say('Missing Url') 
+end 
 
--- save url into datababase
-local ok, err = db:save(long_url, short_url)
+local res, err = db:find_long_one(long_url)
 
---TODO: avoid sql injection attack
-
-if res then ngx.say(config.url .. '/' .. short_url) end
+if res and  #res > 0 then
+  local data = res[1]
+  ngx.say(config.url .. '/' .. data.short_url) 
+else
+  local short_url = util.shorten(long_url) 
+  local ok, err   = db:save(long_url, short_url)
+  if ok then 
+    ngx.say(config.url .. '/' .. short_url) 
+  else 
+    ngx.log(ngx.ERR, 'Failed to save url to db ' .. err)
+    ngx.say(err) 
+  end
+end
