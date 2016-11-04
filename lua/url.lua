cbd3cceb717a6995cc5b7cf0e6a5471811f99f73
Jeffry Lunggot <jeffry@bayau.com>
2016-11-04 17:49:01 +0800

    refactor code

--- lua/url.lua
+++ lua/url.lua
@@ -11,16 +11,17 @@ end
 
 local res, err = db:find_long_one(long_url)
 
+-- we have record of the long url
 if res and  #res > 0 then
-  local data = res[1]
-  ngx.say(config.url .. '/' .. data.short_url) 
-else
-  local short_url = util.shorten(long_url) 
-  local ok, err   = db:save(long_url, short_url)
-  if ok then 
-    ngx.say(config.url .. '/' .. short_url) 
-  else 
-    ngx.log(ngx.ERR, 'Failed to save url to db ' .. err)
-    ngx.say(err) 
-  end
+  return ngx.say(config.url .. '/' .. res[1].short_url) 
+end
+
+-- create new entry
+local short_url = util.shorten(long_url) 
+local ok, err   = db:save(long_url, short_url)
+if ok then 
+  ngx.say(config.url .. '/' .. short_url) 
+else 
+  ngx.log(ngx.ERR, 'Failed to save url to db ' .. err)
+  ngx.say(err) 
 end
